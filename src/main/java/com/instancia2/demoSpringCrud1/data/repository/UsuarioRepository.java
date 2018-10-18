ea3e9f47e106181098fb7528743e81b0e8c90eaf
Jorge Lopez <pruebas@instancia2.es>
2018-10-18 02:22:56 +0200

    Añadimos comentarios

--- src/main/java/com/instancia2/demoSpringCrud1/data/repository/UsuarioRepository.java
+++ src/main/java/com/instancia2/demoSpringCrud1/data/repository/UsuarioRepository.java
@@ -9,14 +9,37 @@ import java.util.List;
 @Repository
 public interface UsuarioRepository extends JpaRepository<UsuarioEntity,Long> {
 
+    /**
+     * Listamos todos los usuarios
+     * @return
+     */
     List<UsuarioEntity> findAll();
 
+    /**
+     * Busca un usuario dado su id
+     * @param id
+     * @return
+     */
     UsuarioEntity findUsuarioEntityByIdUsuario(Long id);
 
+    /**
+     * Busca un usuario dado su nombre
+     * @param name
+     * @return
+     */
     UsuarioEntity findUsuarioEntityByName(String name);
 
+    /**
+     * Guarda en base de datos un nuevos usuario
+     * @param usuarioEntity
+     * @return
+     */
     UsuarioEntity save(UsuarioEntity usuarioEntity);
 
+    /**
+     * Borra de la base de datos un usuario dado su id
+     * @param id
+     */
     void deleteUsuarioEntityByIdUsuario(Long id);
 
 }
