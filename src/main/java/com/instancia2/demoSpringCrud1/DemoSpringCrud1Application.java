96bb32889f8b2290996192fa08dc702519a6f0b7
jorgiom4 <jorgiom4@01>
2018-10-29 21:19:27 +0100

    Configuramos connectividad para microservicios

--- src/main/java/com/instancia2/demoSpringCrud1/DemoSpringCrud1Application.java
+++ src/main/java/com/instancia2/demoSpringCrud1/DemoSpringCrud1Application.java
@@ -2,7 +2,10 @@ package com.instancia2.demoSpringCrud1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+
+@EnableEurekaClient
 @SpringBootApplication
 public class DemoSpringCrud1Application {
 
